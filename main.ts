9d5a584cb651f6ae87ceeb6a107c940f9433c181
Carlos "Café Café" García <58571285+Carlos-CafeCafe@users.noreply.github.com>
2023-07-11 19:01:58 -0300

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -7,11 +7,13 @@ input.onButtonPressed(Button.B, function () {
 function medirHumedad () {
     Humedad = pins.analogReadPin(AnalogPin.P0)
     basic.showNumber(Humedad)
+    if (Humedad >= 99) {
+        radio.sendString("tanqueLleno")
+    }
 }
 let Humedad = 0
 radio.setGroup(1)
 Humedad = 0
 loops.everyInterval(5000, function () {
     medirHumedad()
-    radio.sendValue("Humedad", Humedad)
 })
